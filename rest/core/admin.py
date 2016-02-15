9c52f41786e6f4a5dfb29a094bf0725a4b40716f
philippe28 <phadc@hotmail.com>
2016-02-15 10:25:28 -0200

    admin
    coreção no models e urls

--- rest/core/admin.py
+++ rest/core/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Produto
+
+# Register your models here
+
+"""
+@admin.register(Produto)
+class TrackAdmin(admin.ModelAdmin):
+    list_display = ('cpu', 'memoria', 'disco', 'so', 'preco', 'provedores')
+"""
+
+
+admin.site.register(Produto)
--- rest/core/models.py
+++ rest/core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse_lazy
 
 
 class Produto(models.Model):
@@ -7,12 +8,17 @@ class Produto(models.Model):
     memoria = models.IntegerField(default=4)
     disco = models.IntegerField(default=250)
     so = models.CharField(max_length=50)
-    preco = models.FloatField()
+    preco = models.DecimalField(max_digits=9, decimal_places=2)
     provedores = models.CharField(max_length=150)
+    release = models.DateTimeField(u'lançamento')
 
     class Meta:
         verbose_name = 'Produto'
         verbose_name_plural = 'Produtos'
+        ordering = ['preco']
 
-    def __unicode__(self):
-        return self.titulo
+    def get_absolute_url(self):
+        return reverse_lazy('produto_detales', kwargs={'pk': self.pk})
+
+    def __str__(self):
+        return 'Produto: ' + self.provedores
